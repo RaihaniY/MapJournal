f4cee8290c3f40ebca1dd3b2558e56806e635ae5
Yoseph Raihani <yosephraihani@gmail.com>
2022-03-07 15:47:56 -0600

    Initial Commit

---
+++ mapJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mapJournal
+//
+//  Created by Yoseph Raihani on 3/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
